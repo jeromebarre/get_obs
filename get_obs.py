b5389d3c213bfef0e9be46d27adada5f3c16ad77
jeromebarre <barre@ucar.edu>
2022-05-06 21:28:29 +0000

    add stuff

--- get_obs.py
+++ get_obs.py
@@ -19,7 +19,7 @@ from pathlib import Path
 import yaml as ym
 
 class obs_win(object):
-    def __init__(self, sta, end, win, pfm, ins, obv, pio, pbd, cln):
+    def __init__(self, sta, end, win, pfm, ins, obv, pio, pbd, cln, cch):
         self.sta = sta
         self.end = end
         self.win = win
@@ -31,9 +31,11 @@ class obs_win(object):
         version = sys.version.split('.')[0]+'.'+sys.version.split('.')[1]
         os.environ["PYTHONPATH"] = self.pbd+'/lib/python'+version+'/pyioda/ioda/../:/usr/local/lib'
         self.cln = cln
+        self.cch = cch
         self.tmpdir = Path(__file__).parent/str('tmp_'+self.pfm+'_'+self.ins+'_'+self.obv)
         self.get_win_range()
-        if self.ins == 'MODIS': self.getnconv_modis() 
+        if self.ins == 'MODIS': self.getnconv_modis()
+        if self.ins == 'VIIRS': self.getnconv_viirs()
         
 
     def get_win_range(self):
@@ -48,12 +50,74 @@ class obs_win(object):
         if finish: exit()
         if not os.path.isdir(self.pio):Path(self.pio).mkdir()
 
+    def getnconv_viirs(self):
+        '''
+        obs ingest function for VIIRS NOAA archive product (not NRT)
+        it will work with ordering the data first on
+        https://www.avl.class.noaa.gov/saa/products/search?datatype_family=JPSS_GRAN
+        and then using ftp path provided by NOAA CLASS
+        e.g. ftp.avl.class.noaa.gov/<order#>/<suborder#>/001/
+        '''
+        if self.pfm == "NPP": pref = "npp"
+        if self.pfm == "NOAA-20": pref = "j01"
+        ordr_f = Path(__file__).parent/'order_file'
+        if not os.path.isfile(ordr_f) or not self.cch:
+            order = input("Enter order numbers provided by CLASS order"+ \
+                    +"(next to the cd command on the order email):")
+            with open(ordr_f, 'w') as f: f.write(order)
+        else:
+            with open(ordr_f, 'r') as f: order = f.read()
+        f.close()
+        #not sure for now
+        order = order+'/001/'
+        ftp = ' ftp://anonymous:psswd@ftp.avl.class.noaa.gov/'+order
+        cmd = 'wget -r -nc -nd -np -nv -A '
+        exe = Path(self.pbd)/'bin'/'viirs_aod2ioda.py '
+        #get the files hourly for now (lazy coding)
+        for w_s,w_e in zip(self.lwin_s,self.lwin_e):
+            finish = False
+            if w_s == self.lwin_s[-1]: finish = True
+            self.check_clean(finish)
+            w_c = w_s
+            while w_c < w_e:
+                w_c_p1 = w_c + Timedelta(hours=1)
+                w_c_m1 = w_c + Timedelta(hours=-1)
+                ymdh = w_c.strftime('%Y') + w_c.strftime('%m') + \
+                        w_c.strftime('%d') + w_c.strftime('%H')
+                ymdh_p1 = w_c_p1.strftime('%Y') + w_c_p1.strftime('%m') + \
+                        w_c_p1.strftime('%d') + w_c_p1.strftime('%H')
+                ymdh_m1 = w_c_m1.strftime('%Y') + w_c_m1.strftime('%m') + \
+                        w_c_m1.strftime('%d') + w_c_m1.strftime('%H')
+
+                fnam = ' "JRR-AOD_*_'+pref+'_s'+ymdh+'*_e'+ymdh+'*_c*.tar,'+    \
+                          'JRR-AOD_*_'+pref+'_s'+ymdh_m1+'*_e'+ymdh+'*_c*.tar,'+ \
+                          'JRR-AOD_*_'+pref+'_s'+ymdh+'*_e'+ymdh_p1+'*_c*.tar" '
+
+                locf = ' -P '+str(self.tmpdir)
+
+                fcmd = cmd + fnam + ftp + locf
+                print(fcmd)
+                #os.system(fcmd)
+                w_c = w_c + Timedelta(hours=1)
+            os.system('cat *.tar | tar -xvf - -i')
+            w_m = w_s + self.win//2
+            ymdh_m = w_m.strftime("%Y%m%d%H")
+            fout = self.pio+'/'+self.ins+'_'+self.pfm+'_'+ymdh_m+'.nc'
+            #thining set to 0.1 (10%) for space and speed
+            os.system(str(exe)+'-i '+str(self.tmpdir)+'/*.nc -n 0.1 -m nesdis -k maskout -o '+fout)
+
+
+
     def getnconv_modis(self):
+        '''
+        obs ingest function for MODIS NASA product
+        on https://ladsweb.modaps.eosdis.nasa.gov
+        '''
         if self.pfm == "Terra": pref="MOD04_L2"
         if self.pfm == "Aqua": pref="MYD04_L2"
 
         tokfile = Path(__file__).parent/'eosdis_token'
-        if not os.path.isfile(tokfile):
+        if not os.path.isfile(tokfile) or not self.cch:
             tok = input("Enter token from https://ladsweb.modaps.eosdis.nasa.gov/: ")
             with open(tokfile, 'w') as f: f.write(tok)
         else:
@@ -62,7 +126,7 @@ class obs_win(object):
 
         hdrp = ' --header "Authorization: Bearer ' +tok+'" '
         url = 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/'
-        cmd = 'wget -e robots=off -m -nv -np --reject html,tmp -nH --cut-dirs=6 -A '
+        cmd = 'wget -e robots=off -m -nc -nv -np --reject html,tmp -nH --cut-dirs=6 -A '
         exe = Path(self.pbd)/'bin'/'modis_aod2ioda.py '
         for w_s,w_e in zip(self.lwin_s,self.lwin_e):
             finish = False
@@ -83,15 +147,10 @@ class obs_win(object):
                 w_c = w_c + Timedelta(minutes=5)
 
             w_m = w_s + self.win//2
-            #$binioda/modis_aod2ioda.py -i tmp_Terra_MODIS_AOD/* -t 2021080100 -p Terra -o test.nc
             ymdh = w_m.strftime("%Y%m%d%H")
             fout = self.pio+'/'+self.ins+'_'+self.pfm+'_'+ymdh+'.nc'
             os.system(str(exe)+'-i '+str(self.tmpdir)+'/* -t '+ymdh+' -p '+self.pfm+' -o '+fout)
 
-
-
-
-            
 
 
 def main():
@@ -109,6 +168,7 @@ def main():
 
     args = parser.parse_args()
     ymlist = ym.load(open(args.yaml_file),Loader=ym.FullLoader)
+    #ideally we would want to pass data structures to reduce args passed in class
     sta = ymlist["start date"]
     end = ymlist["end date"]
     win = Timedelta(ymlist["window length"])
@@ -121,9 +181,9 @@ def main():
     pbd = ymlist["path build"]
 
     cln = ymlist["clean"]
+    cch = ymlist["cache"]
 
-
-    owclass = obs_win(sta, end, win, pfm, ins, obv, pio, pbd, cln)
+    owclass = obs_win(sta, end, win, pfm, ins, obv, pio, pbd, cln, cch)
 
     
 
